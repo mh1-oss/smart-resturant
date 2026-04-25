8d4775f4bdfbcb22a49bc13552100c078690ed4d
mh1-oss <mmhhoo967.mm@gamil.com>
2026-04-25 22:13:00 +0300

    6.4.3

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const connectionString = process.env.DATABASE_URL!;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool as any);
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    // @ts-ignore
+    adapter: adapter as any,
     log: ["error"],
   });
 
